f27ad77ad0aaf66191a34453cd038e662ce1c5ec
sspark <sspark@tpirates.com>
2023-02-28 17:52:21 +0900

    실행 결과 주석문 추가

--- args.py
+++ args.py
@@ -4,6 +4,12 @@ def test_var_args(f_arg, *argv):
         print("*argv 인자", arg)
 
 test_var_args('야숩', 'python', '달걀', '테에스트')
+'''결과
+첫 인자: 야숩
+*argv 인자 python
+*argv 인자 달걀
+*argv 인자 테에스트
+'''
 
 def greet_me(**kwargs):
     if kwargs is not None:
@@ -11,7 +17,9 @@ def greet_me(**kwargs):
             print("%s == %s" % (key, value))
 
 greet_me(name="yasoob")
-
+'''결과
+name == yasoob
+'''
 
 def test_args_kwargs(arg1, arg2, arg3):
         print ("인자1:", arg1)
@@ -20,6 +28,16 @@ def test_args_kwargs(arg1, arg2, arg3):
 
 args = ("two", 3, 5)
 test_args_kwargs(*args)
+'''결과
+인자1: two
+인자2: 3
+인자3: 5
+'''
 
 kwargs = {"arg3": 3, "arg2": "two", "arg1": 5}
 test_args_kwargs(**kwargs)
+'''결과
+인자1: 5
+인자2: two
+인자3: 3
+'''
